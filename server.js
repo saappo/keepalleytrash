e68f900ce5ace8080a40b424c40b77abe9422714
saappo <jerisq@saappo.com>
2025-06-28 11:48:18 -0500

    Fix: export app for Vercel and only call app.listen locally

--- server.js
+++ server.js
@@ -506,6 +506,10 @@ app.use((err, req, res, next) => {
   });
 });
 
-app.listen(PORT, () => {
-  console.log(`Server is running on port ${PORT}`);
-}); 
+if (process.env.NODE_ENV !== 'production') {
+  app.listen(PORT, () => {
+    console.log(`Server is running on port ${PORT}`);
+  });
+}
+
+module.exports = app; 
